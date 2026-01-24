c4b49ddc5c99c4e035463a835e9a07f4d6d94942
IvyJebet <jebetivy388@gmail.com>
2026-01-24 07:46:18 -0600

    Derver update

--- server.py
+++ server.py
@@ -33,7 +33,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -111,8 +111,6 @@ class MarketListing(BaseModel):
 
 class SellerCheck(BaseModel):
     phone: str
-
-# 1. UPDATE THE MODEL
 class SellerRegistration(BaseModel):
     name: str
     phone: str
